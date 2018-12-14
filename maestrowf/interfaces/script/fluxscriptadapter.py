2edad230ab0b9a4eb0afc39195b62cf7ffbc64cd
Francesco Di Natale <dinatale3@llnl.gov>
2018-12-14 15:07:32 -0800

    Addition of chmod when wrapping main script.

--- maestrowf/interfaces/script/fluxscriptadapter.py
+++ maestrowf/interfaces/script/fluxscriptadapter.py
@@ -33,6 +33,7 @@ import logging
 import os
 import re
 import json
+import stat
 import subprocess as sp
 
 from maestrowf.abstracts.interfaces import SchedulerScriptAdapter
@@ -865,6 +866,7 @@ class FluxScriptAdapter(SchedulerScriptAdapter):
             script.write(cmd)
 
         if to_be_scheduled:
+            os.chmod(script_path, st.st_mode | stat.S_IXUSR)
             w_script = os.path.join(
                 ws_path,
                 "{}.wrapper.sh".format(step.name)
@@ -893,6 +895,7 @@ class FluxScriptAdapter(SchedulerScriptAdapter):
                 script.write(cmd)
 
             if to_be_scheduled:
+                os.chmod(script_path, st.st_mode | stat.S_IXUSR)
                 w_script = os.path.join(
                     ws_path,
                     "{}.wrapper.sh".format(step.name)
