b622e62b0585e318dc4cff5f6901a7ae9d619eda
Francesco Di Natale <dinatale3@llnl.gov>
2020-03-09 22:12:55 -0700

    First pass implementation of DryRunAdapter.

--- maestrowf/interfaces/script/psuedoadapters.py
+++ maestrowf/interfaces/script/psuedoadapters.py
@@ -1,16 +1,50 @@
-import getpass
+"""A module containing psuedoasdapters for miscellaneous functionality."""
+from collections import defaultdict
+from hashlib import md5
 import logging
-import os
-import re
 
-from maestrowf.abstracts.interfaces import SchedulerScriptAdapter
+from maestrowf.abstracts.interfaces import ScriptAdapter
 from maestrowf.abstracts.enums import JobStatusCode, State, SubmissionCode, \
     CancelCode
 from maestrowf.interfaces import ScriptAdapterFactory
 from maestrowf.interfaces.script import CancellationRecord, SubmissionRecord
-from maestrowf.utils import start_process
 
 LOGGER = logging.getLogger(__name__)
 
-class DryRunAdapter(SchedulerScriptAdapter):
+
+class DryRunAdapter(ScriptAdapter):
     """A pseudoadapter capable of monkeypatching itself to provide dry run."""
+
+    key = "dryrun"
+
+    def __init__(self, **kwargs):
+        """
+        Initialize an instance of the DryRunAdapter.
+
+        The DryRunAdapter provides Maestro's support for dry run.
+        This adapter works primarily by taking in a complete batch dictionary
+        and wrapping the implementation to write scripts and then mocking all
+        scheduler calls to mimic success.
+
+        :param **kwargs: A dictionary with default settings for the adapter.
+        """
+
+        super(DryRunAdapter, self).__init__(**kwargs)
+
+        # Monkey patch in the methods from the appropriate adapter class
+        # that would have been executed.
+        adapter_type = kwargs.get("type", "local")
+        self._adapter = ScriptAdapterFactory.get_adapter(adapter_type)
+
+    def cancel_jobs(self, joblist):
+        return CancellationRecord(CancelCode.OK, 0)
+
+    def check_jobs(self, joblist):
+        return JobStatusCode.OK, defaultdict(State.FINISHED)
+
+    def _write_script(self, ws_path, step):
+        return self._adapter._write_script(ws_path, step)
+
+    def submit(self, step, path, cwd, job_map=None, env=None):
+        return SubmissionRecord(
+            SubmissionCode.OK, 0, md5(step.name).hexdigest())
